800885bca6bfb699d039aa401a1f427ad1d8273c
Haoyang Gao <gaohaoyang126@126.com>
2024-05-13 10:51:28 -0400

    feat: 🎸 update data

--- src/constants/wfdData.ts
+++ src/constants/wfdData.ts
@@ -12,9 +12,204 @@ export const wfdData = [
     sentence: 'I want to make an appointment with the manager.',
   },
   {
-    sentence: 'In my opinion, this car should be repaired soon.',
+    sentence: 'The weather has been lovely from this time of the year.',
+  },
+  {
+    sentence: 'Please close the door behind you, when you leave the room.',
+  },
+  {
+    sentence: 'There was a lot of traffic this morning.',
+  },
+  {
+    sentence: 'The restroom is down the hall and on the right.',
+  },
+  {
+    sentence: 'The teacher explains the homework to the students.',
+  },
+  {
+    sentence: 'At that time, few people moved through the town to the village.',
+  },
+  {
+    sentence: 'The fiction books have just passed to the counter.',
+  },
+  {
+    sentence: 'The fiction books have just past to the counter.',
+  },
+  {
+    sentence: 'You have to make an appointment with your doctor.',
+  },
+  {
+    sentence: 'The students will meet their new teacher after summer vacation.',
+  },
+  {
+    sentence: 'Please move us to the meeting room for the next hour.',
+  },
+  {
+    sentence: 'You must call your doctor to make an appointment.',
+  },
+  {
+    sentence: 'Please make an appointment with your tutor about work.',
   },
   {
     sentence: 'Rail transport is becoming more and more popular.',
+  },
+  {
+    sentence: 'You can keep your bags in the back room.',
+  },
+  {
+    sentence: 'The weather used to be lovely at this time of the year.',
+  },
+  {
+    sentence: 'Everyone in this room needs to follow the rules.',
+  },
+  {
+    sentence: 'Before preparing foods please make sure to wash your hands.',
+  },
+  {
+    sentence: 'Before preparing food please make sure to wash your hands.',
+  },
+  {
+    sentence: 'The teacher explained the homework to the class.',
+  },
+  {
+    sentence: 'You should meet me in the lecture theater room.',
+  },
+  {
+    sentence: 'At that time, few people moved from towns to villages.',
+  },
+  {
+    sentence: 'Students of the first year usually live on campus.',
+  },
+  {
+    sentence: 'The fiction books are just past the counter.',
+  },
+  {
+    sentence: 'The teacher asked the group to complete the task.',
+  },
+  {
+    sentence: 'There were a lot of traffic jams this morning.',
+  },
+  {
+    sentence: 'The school canteen sells a large variety of water and food.',
+  },
+  {
+    sentence: 'Please turn off the lights to save the energy.',
+  },
+  {
+    sentence: 'Traffic is the main cause of air pollution in many cities.',
+  },
+  {
+    sentence: 'You will get your uniform on the first day.',
+  },
+  {
+    sentence: 'In my opinion, this car should be repaired soon.',
+  },
+  {
+    sentence: 'We should have a meeting to discuss and report.',
+  },
+  {
+    sentence: 'Sales figures for last year were better than expected.',
+  },
+  {
+    sentence: 'The university canteen offers different healthy meal options.',
+  },
+  {
+    sentence: 'Employment opportunities available in engineering are increasing rapidly.',
+  },
+  {
+    sentence: 'The employment opportunities available in engineering are increasing rapidly.',
+  },
+  {
+    sentence: 'We cannot consider an increase in price at this stage.',
+  },
+  {
+    sentence: 'We should have a meeting to discuss the report.',
+  },
+  {
+    sentence: 'Our culture influences the choices we make.',
+  },
+  {
+    sentence: 'There will be a guest lecturer in the next class.',
+  },
+  {
+    sentence: 'Exam results will be published on the noticeboard.',
+  },
+  {
+    sentence: 'Atoms are the central building blocks of matter.',
+  },
+  {
+    sentence: 'There will be a meeting for the first-year students on Friday.',
+  },
+  {
+    sentence: 'The stairs are to the left of the elevator.',
+  },
+  {
+    sentence: 'Restrooms are down the entrance and to the right.',
+  },
+  {
+    sentence: 'Time is needed to complete the lecture.',
+  },
+  {
+    sentence: 'Restaurants are down to the hall and next to the right.',
+  },
+  {
+    sentence: 'Make sure you wash your hands before preparing the food.',
+  },
+  {
+    sentence: 'You should draw your graph on a separate page.',
+  },
+  {
+    sentence: 'You can ask your tutor for further assistance.',
+  },
+  {
+    sentence: 'Speak to your tutor if you need further assistance.',
+  },
+  {
+    sentence: 'Talk to your tutor if you need further assistance.',
+  },
+  {
+    sentence: 'Talk to your tutor if you require further assistance.',
+  },
+  {
+    sentence: 'Speak to your tutor if you require further assistance.',
+  },
+  {
+    sentence: 'You are trained to be a special journalist.',
+  },
+  {
+    sentence: 'We can have a lecture on the morning of Thursday.',
+  },
+  {
+    sentence: 'We can all meet in the office after the lecture.',
+  },
+  {
+    sentence: 'The digital revolution has changed the way we read.',
+  },
+  {
+    sentence: 'Before the refrigerator was invented, people used underground ice houses.',
+  },
+  {
+    sentence: 'The nation achieved prosperity by opening its ports for trade.',
+  },
+  {
+    sentence: 'Understanding visual media has never been more challenging.',
+  },
+  {
+    sentence: 'Online courses allow students to work on their own paces.',
+  },
+  {
+    sentence: 'In my opinion, this car should be repaired soon.',
+  },
+  {
+    sentence: 'You can get your coffee and tea in the lounge room.',
+  },
+  {
+    sentence: 'The teacher asked the group to commence the task. ',
+  },
+  {
+    sentence: 'The island is located at the south end of the bay.',
+  },
+  {
+    sentence: 'The falling birth rate means the number of students drops.',
   },
 ]
